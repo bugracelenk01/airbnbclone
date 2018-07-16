a58d767b3017ad8562d5a9878f1b0961d8ac503e
Bugra Celenk <bugracelenk01@gmail.com>
2018-07-17 02:27:56 +0300

    added password toggle show password button

--- src/components/form/InputField.js
+++ src/components/form/InputField.js
@@ -11,8 +11,22 @@ import {
 } from 'react-native';
 
 export default class InputField extends Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            secureInput : props.inputType === 'text' || props.inputType === 'email' ? false : true,
+        }
+        this.toggleShowPassword = this.toggleShowPassword.bind(this);
+    }
+
+    toggleShowPassword() {
+        this.setState({ secureInput: !this.state.secureInput });
+    }
+
     render() {
         const { labelText, labelTextSize, labelColor, textColor, borderBottomColor, inputType, customStyle } = this.props;
+        const { secureInput } = this.state;
         const fontSize = labelTextSize || 14;
         const color = labelColor || colors.white;
         const inputColor = textColor || colors.white;
@@ -20,10 +34,18 @@ export default class InputField extends Component{
         return(
             <View style={[customStyle, styles.wrapper]}>
                 <Text style={[{fontSize, color},styles.label]}>{labelText}</Text>
+                {inputType === 'password' ? 
+                    <TouchableOpacity 
+                        style={styles.showButton}
+                        onPress={this.toggleShowPassword}
+                    >
+                        <Text style={styles.showButtonText}>{secureInput ? 'Show' : 'Hide'}</Text>
+                    </TouchableOpacity>
+                : null}
                 <TextInput 
                     autoCorrect= {false}
                     style={[{color:inputColor, borderBottomColor: borderBottom }, styles.inputField]}
-                    secureTextEntry={inputType === 'password'}
+                    secureTextEntry={secureInput}
                 />
             </View>
         );
@@ -52,5 +74,13 @@ const styles = StyleSheet.create({
         borderBottomWidth: 1,
         paddingTop: 5,
         paddingBottom: 5,
+    },
+    showButton: {
+        position: 'absolute',
+        right: 0,
+    },
+    showButtonText: {
+        color: colors.white,
+        fontWeight: '700',
     }
 });
